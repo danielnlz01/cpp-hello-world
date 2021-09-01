c88f6e5177c62e745b8347c4ad9376c77e474ff3
danielnlz01 <38385574+danielnlz01@users.noreply.github.com>
2021-08-31 20:41:54 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+using namespace std;
+//siempre se pone
+
+int main()
+{
+  cout << "Hello World!" << endl;
+  return 0;
+}
+// cout para imprimir, endl para salto de linea
+
